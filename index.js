1da30adce198fc0930ca2cedf9662c0b72a34ae0
iam-PJ <prathmeshjagtap3636@gmail.com>
2025-01-24 17:38:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('welcome to Goa Singham..... Shauk laga ..tu yaha aya nahi ..tuje yaha laya gaya hai...ali re ali ata tuzi bari ali.. singhaaammmm ');
+ res.send('welcome to Goa Singham..... Shauk laga ..tu yaha aya nahi ..tuje yaha laya gaya hai...ali re ali ata tuzi bari ali.. singhaaam ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
